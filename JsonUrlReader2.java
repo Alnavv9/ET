89cab71f790409b806ee5c99d8cff934366f4f45
Alnavv9 <89711034+Alnavv9@users.noreply.github.com>
2023-07-03 03:30:20 -0400

    Add files via upload

---
+++ JsonUrlReader2.java
@@ -0,0 +1,135 @@
+package org.apache.beam.examples;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+import org.apache.beam.examples.ExpresoSantiago;
+
+public class JsonUrlReader2 {
+
+    public JsonUrlReader2() {
+
+    }
+
+    public static void main(String[] args) throws DatabindException, MalformedURLException, IOException {
+        JsonUrlReader2 j = new JsonUrlReader2();
+        ArrayList<ExpresoSantiago> Atp = j.cargarURLx();
+
+        for (ExpresoSantiago tp : Atp) {
+            System.out.println(tp);
+        }
+    }
+
+    public ArrayList<ExpresoSantiago> cargarURLx() throws StreamReadException, DatabindException, MalformedURLException, IOException {
+        String url_recorrido = "https://www.red.cl/restservice_v2/rest/getservicios/all";
+        ArrayList<ExpresoSantiago> Aes = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode elementos_recorrido = mapper.readTree(new URL(url_recorrido));
+
+        //AGREGANDO DATOS IDA
+        for (JsonNode recorrido_x : elementos_recorrido) {
+            String url = "https://www.red.cl/restservice_v2/rest/conocerecorrido?codsint=" + recorrido_x.asText(); //API EXPRESO DE SANTIAGO
+            JsonNode elementos = mapper.readTree(new URL(url));
+            JsonNode negocio = elementos.get("negocio");
+            JsonNode ida = elementos.get("ida");
+            JsonNode regreso = elementos.get("regreso");
+
+            if (ida == null) {
+                ExpresoSantiago es = new ExpresoSantiago();
+                es.setRecorrido(recorrido_x.asText());
+                es.setId_empresa(negocio.get("id").asText());
+                es.setNombre_empresa(negocio.get("nombre").asText());
+                es.setId_ida("N/A");
+                es.setTipo_dia("N/A");
+                es.setInicio_dia("N/A");
+                es.setFin_dia("N/A");
+                es.setDestino("N/A");
+                es.setParadero("N/A");
+                es.setCod_paradero("N/A");
+                es.setComuna_paradero("N/A");
+                System.out.println(es);
+                Aes.add(es);
+            } else {
+                JsonNode ida_horario = ida.get("horarios");
+                if (ida_horario.isArray()) {
+                    for (JsonNode horario : ida_horario) {
+                        JsonNode paradero_ida = ida.get("paraderos");
+
+                        for (JsonNode paradero : paradero_ida) {
+                            ExpresoSantiago es = new ExpresoSantiago();
+                            es.setRecorrido(recorrido_x.asText());
+                            es.setId_empresa(negocio.get("id").asText());
+                            es.setNombre_empresa(negocio.get("nombre").asText());
+                            es.setId_ida(ida.get("id").asText());
+                            es.setTipo_dia(horario.get("tipoDia").asText());
+                            es.setInicio_dia(horario.get("inicio").asText());
+                            es.setFin_dia(horario.get("fin").asText());
+                            es.setDestino(ida.get("destino").asText());
+                            es.setParadero(paradero.get("name").asText());
+                            es.setCod_paradero(paradero.get("cod").asText());
+                            es.setComuna_paradero(paradero.get("comuna").asText());
+                            System.out.println(es);
+                            Aes.add(es);
+                        }
+
+                    }
+                }
+            }
+            if (regreso == null) {
+                ExpresoSantiago es = new ExpresoSantiago();
+                es.setRecorrido(recorrido_x.asText());
+                es.setId_empresa(negocio.get("id").asText());
+                es.setNombre_empresa(negocio.get("nombre").asText());
+                es.setId_ida("N/A");
+                es.setTipo_dia("N/A");
+                es.setInicio_dia("N/A");
+                es.setFin_dia("N/A");
+                es.setDestino("N/A");
+                es.setParadero("N/A");
+                es.setCod_paradero("N/A");
+                es.setComuna_paradero("N/A");
+                System.out.println(es);
+                Aes.add(es);
+            } else {
+                JsonNode regreso_horario = regreso.get("horarios");
+                //AGREGANDO DATOS VUELTA
+                if (regreso_horario.isArray()) {
+                    for (JsonNode horario : regreso_horario) {
+                        JsonNode paradero_regreso = regreso.get("paraderos");
+
+                        for (JsonNode paradero : paradero_regreso) {
+                            ExpresoSantiago es = new ExpresoSantiago();
+                            es.setRecorrido(recorrido_x.asText());
+                            es.setId_empresa(negocio.get("id").asText());
+                            es.setNombre_empresa(negocio.get("nombre").asText());
+                            es.setId_ida(regreso.get("id").asText());
+                            es.setTipo_dia(horario.get("tipoDia").asText());
+                            es.setInicio_dia(horario.get("inicio").asText());
+                            es.setFin_dia(horario.get("fin").asText());
+                            es.setDestino(regreso.get("destino").asText());
+                            es.setParadero(paradero.get("name").asText());
+                            es.setCod_paradero(paradero.get("cod").asText());
+                            es.setComuna_paradero(paradero.get("comuna").asText());
+                            System.out.println(es);
+                            Aes.add(es);
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+        return Aes;
+    }
+}
